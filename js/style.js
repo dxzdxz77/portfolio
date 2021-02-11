5a7c5114ea6783ec66c052d78fa91d5bb4154e17
dxzdxz77 <ehqntz@naver.com>
2021-02-11 22:36:35 +0900

    123

--- js/style.js
+++ js/style.js
@@ -1,15 +1,15 @@
 $(function(){
-        var sta = 1
+        /*var sta = 1
         $('.open').click(function(){
 
           if (sta == 1){
           $(this).addClass('active');
-          $('.btn1').animate({top:'-70px'},500);
-          $('.btn2').animate({top:'-120px'},500);
-          $('.btn3').animate({top:'-170px'},500);
-          $('.btn4').animate({top:'-220px'},500);
-          $('.btn5').animate({top:'-270px'},500);
-          $('.btn6').animate({top:'-320px'},500);
+          $('.btn1').animate({top:'70px'},500);
+          $('.btn2').animate({top:'120px'},500);
+          $('.btn3').animate({top:'170px'},500);
+          $('.btn4').animate({top:'220px'},500);
+          $('.btn5').animate({top:'270px'},500);
+          $('.btn6').animate({top:'320px'},500);
           $('.btns a').css({opacity: 1});
           sta = 2
           }else{
@@ -21,8 +21,16 @@ $(function(){
         })//click
 
         $(window).scroll(function(){
-            if($(document).scrollTop() >= 50 && $(document).scrollTop() <= $('#footer').offset().top - 500){
+            if($(document).scrollTop() >= 50 && $(document).scrollTop() <= $('#footer').offset().top - 1000){//푸터에서 맨 위쪽(top)까지의 거리를 크게 주면 푸터 위에서 버튼 사라짐
                 $('.open').fadeIn(200)
             }else {$('.open').fadeOut(200)}
-        })//scroll
+        })//scroll 네비 돌아가면서 내려오기*/
+
+        $('#header .all_nav').click(function(){
+          $('#nav_pop').fadeIn();
+      })
+
+     $('#nav_pop .nav_close').click(function(){
+         $('#nav_pop').fadeOut();
+     })//네비 팝업   
 })//js end
